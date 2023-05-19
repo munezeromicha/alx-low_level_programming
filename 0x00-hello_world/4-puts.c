1624b825757ad934e20778ca91d4f5b5aeb716b7
munezeromicha <munezerontaganiramichel@gmail.com>
2023-05-18 18:38:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+<include stdio.h>
+\**
+*I'm going to use main function.
+*Program will return 0
+*/
+int main(void){
+	puts("\n"Programming is like building a multilingual puzzle");
+	return(0);
+}
