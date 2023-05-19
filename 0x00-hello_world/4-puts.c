9ee3d82f8d4f00c1223a3ff4afbc0a5458aa454d
munezeromicha <munezerontaganiramichel@gmail.com>
2023-05-18 19:37:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+
 /**
- * I'm going to use  main  function only as an Entry point
+ * main - Entry point
  *
- * The program will always Return 0
+ * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
