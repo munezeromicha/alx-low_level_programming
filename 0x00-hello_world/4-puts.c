21e10c1944224afaf3facadb0c9bfaec7649574c
munezeromicha <munezerontaganiramichel@gmail.com>
2023-05-18 19:19:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-<include stdio.h>
-/*
-*I'm going to use main function only.
-*Program will return0
-*Program solved
-*
-* This is C programing language
-* This betty commenting style.
-*/
-int main(void){
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
+
