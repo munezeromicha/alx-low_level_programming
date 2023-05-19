18501ed5fb81b475614b9939b7bce85f805ee1d2
munezeromicha <munezerontaganiramichel@gmail.com>
2023-05-18 19:29:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-/**
+/*
  * This is C programming Language
  * I'm going to use  main  function only as an Entry point
- *
  * The program will always Return 0
+ * 
  * Use the function puts
  * You are not allowed to use printf
  */
