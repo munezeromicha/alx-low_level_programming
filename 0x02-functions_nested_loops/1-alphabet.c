ded98bf79fd935100e7084cd813a2671aa76970f
munezeromicha <munezerontaganiramichel@gmail.com>
2023-05-23 14:18:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - verify the source code
+* print_alphabet - function for print_alphabet
+* print_alphabet - function for print_alphabet
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+return (0);
+}
